2a9e5c64bd410aa99df9632f705e173cda11c0f7
unknown <pavanhapse@gmail.com>
2023-01-30 23:41:07 +0530

    Validations applied

--- watch_app/api/serializers.py
+++ watch_app/api/serializers.py
@@ -2,9 +2,14 @@ from rest_framework import serializers
 from watch_app.models import Movie
 
 
+def name_length(value):
+    if len(value) < 2:
+        raise serializers.ValidationError("Name is too Short")
+
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[name_length])
     description = serializers.CharField()
     active = serializers.BooleanField()
 
@@ -18,4 +23,16 @@ class MovieSerializer(serializers.Serializer):
         instace.save()
         return instace
 
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Title and Description should be different")
+        else:
+            return data
 
+        
+"""
+    def validate_name(self, value):        #Field level validation
+        if len(value) < 2:
+            raise serializers.ValidationError("Name is too short")
+        else:
+            return value"""
