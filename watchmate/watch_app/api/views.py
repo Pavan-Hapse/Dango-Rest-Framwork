e5dcd07119a91d8e45c83505c2b48d73d69000d4
unknown <pavanhapse@gmail.com>
2023-02-01 23:50:23 +0530

    Authentication for user

--- watchmate/watch_app/api/views.py
+++ watchmate/watch_app/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ValidationError
 from rest_framework import status
 # from rest_framework.decorators import api_view
@@ -21,9 +22,10 @@ class ReviewCreate(generics.CreateAPIView):
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
         watchlist = WatchList.objects.get(pk=pk)
+        serializer.save(watchList=watchlist)
 
-        review_user = self.request.user
-        review_queryset = Review.objects.filter(watchList=watchlist, review_user=review_user)
+        # review_user = self.request.user
+        # review_queryset = Review.objects.filter(watchList=watchlist, review_user=review_user)
 
         if review_queryset.exists():
             raise ValidationError("You have already reviewed this movie")
@@ -33,15 +35,16 @@ class ReviewCreate(generics.CreateAPIView):
 class ReviewList(generics.ListAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-
-
-class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):  # This class allow us to perform get, update, and delete
-    queryset = Review.objects.all()                     #we can retrive all details only for individual object
-    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         pk = self.kwargs[ 'pk' ]
         Review.objects.filter(watchlist=pk)
+
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):  # This class allow us to perform get, update, and delete
+    queryset = Review.objects.all()  # we can retrive all details only for individual object
+    serializer_class = ReviewSerializer
 
 
 # class ReviewDetail(mixins.RetrieveModelMixin, generics.GenericAPIView):
