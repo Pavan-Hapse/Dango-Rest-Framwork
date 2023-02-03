52b609b5f32abf4e3705ee169f309b101761a556
unknown <pavanhapse@gmail.com>
2023-02-03 13:26:48 +0530

    Permformed Login and Logout Process with help of Token Authentiction

--- watchmate/user_app/api/serializers.py
+++ watchmate/user_app/api/serializers.py
@@ -7,18 +7,25 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2' ]
+        fields = [ 'username', 'email', 'password', 'password2' ]
         extra_kwargs = {
             'password': {'write_only': True}
         }
 
     def save(self):
 
-        password = self.validated_data['password']
-        password2 = self.validated_data['password2']
+        password = self.validated_data[ 'password' ]
+        password2 = self.validated_data[ 'password2' ]
 
         if password != password2:
-            raise serializers.ValidationError({'error':'p1 and p2 should be same'})
+            raise serializers.ValidationError({'error': 'p1 and p2 should be same'})
 
-        if User.objects.filter(email= self.validated_data['email']).exists():
+        if User.objects.filter(email=self.validated_data[ 'email' ]).exists():
             raise serializers.ValidationError({'error': 'Email Already Exists!'})
+
+        account = User(email=self.validated_data[ 'email' ], username=self.validated_data[ 'username' ])
+        account.set_password(password)
+
+        account.save()
+
+        return account
